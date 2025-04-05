33a507da2ddf0b1c426fb6f013423fc7ee0a2e25
amiiiiee <amiameen507@gmail.com>
2025-04-05 12:59:21 +0530

    edits

--- src/pages/editor/ScriptWriting.tsx
+++ src/pages/editor/ScriptWriting.tsx
@@ -9,36 +9,14 @@ const ScriptWriting:React.FC = () => {
   <input 
     type="text" 
     className="text-2xl md:text-3xl lg:text-4xl xl:text-4xl 2xl:text-5xl font-medium bg-transparent outline-none w-full md:w-2/3 text-center border-b-4 border-black border-opacity-30 mb-6 py-2" 
-    value={title}
-    onChange={handleTitleChangee}
+
   />
 </div>;
 
     <div className="flex flex-col ">
       {/* Sidebar Controls - Responsive positioning */}
       <div className="w-full  flex justify-center sticky top-5  z-50 ">
-        <div className="flex justify-center w-[85%] lg:w-4/5 xl:w-[52%] 2xl:w-[45%]  border border-opacity-30  py-7 px-9 gap-4 bg-gray-100 border-black rounded-xl">
-          {/* Scene Action Button */}
 
-          <div className="flex flex-col w-1/3 2xl:w-1/3 space-y-4 ">
-
-            <p  className="text-center text-opacity-70 text-black">Press Keyboard Shift+Tab</p>
-          </div>
-{/* Add Character Button */}
-          <div className="flex flex-col w-1/3 2xl:w-1/3  space-y-4 ">
-
-            <p className="text-center text-opacity-70 text-black">Press Keyboard Tab</p>
-          </div>
-
-                   {/* Add Dialog Button */}
-          <div className="flex flex-col w-1/3 2xl:w-1/3  space-y-4 ">
-
-            <p className="text-center text-opacity-70 text-black">Type Character & Press Enter</p>
-          </div>
-
- 
-      
-        </div>
       </div>
 <div className="w-full flex justify-center">
       {/* Main Content Area - Responsive width and padding */}
