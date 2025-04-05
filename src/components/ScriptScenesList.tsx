be6e3651727ef9481023e9f44f0962151f7ec118
amiiiiee <amiameen507@gmail.com>
2025-04-05 13:06:25 +0530

    edits

--- src/components/ScriptScenesList.tsx
+++ src/components/ScriptScenesList.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import NavigationMenu from '../components/navmenu/NavigationMenu';
-import ScriptsLists from './scriptslists/ScriptsLists';
+import React from "react";
+import NavigationMenu from "../components/navmenu/NavigationMenu";
+import ScriptsLists from "./scriptslists/ScriptsLists";
 
 const ScriptScenesList: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen"> 
+    <div className="flex flex-col min-h-screen">
       <NavigationMenu />
- 
       <div className="flex-1">
         <ScriptsLists />
       </div>
