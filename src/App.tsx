4f30aaf8fa6fd968f7bdf5e386a1211dfd7c5614
amiiiiee <amiameen507@gmail.com>
2025-04-05 12:38:31 +0530

    edits

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-      <AppRoutes />
+        <AppRoutes />
       </PersistGate>
     </Provider>
   );
