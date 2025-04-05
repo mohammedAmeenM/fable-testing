cd560346740893d15f0e797eeae02e1d0a330d52
amiiiiee <amiameen507@gmail.com>
2025-04-05 12:59:43 +0530

    edits

--- src/pages/editor/ScriptWriting.tsx
+++ src/pages/editor/ScriptWriting.tsx
@@ -13,31 +13,7 @@ const ScriptWriting:React.FC = () => {
   />
 </div>;
 
-    <div className="flex flex-col ">
-      {/* Sidebar Controls - Responsive positioning */}
-      <div className="w-full  flex justify-center sticky top-5  z-50 ">
-
-      </div>
-<div className="w-full flex justify-center">
-      {/* Main Content Area - Responsive width and padding */}
-      <div className="w-full  lg:w-4/5  px-4 md:px-8 lg:px-6 mt-4 lg:mt-7 flex flex-col space-y-8">
-      
-
-        {/* Add Scene Button */}
-        <div className="mt-4 flex justify-center pb-8">
-          <button
-            className="border flex  justify-center items-center gap-1  text-lg md:text-lg font-bold border-black border-opacity-30 hover:bg-gray-300 rounded-lg px-4 py-1 bg-white text-black"
-
-          >
-
-            <span className="font-semibold mt-[6px] text-lg">New Scene</span>
-          </button>
-        </div>
-      </div>
-</div>
-
-
-    </div>
+ 
   </div>
   )
 }
