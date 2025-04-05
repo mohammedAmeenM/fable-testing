c404b791f98c50127ceb57bf400e66ac92d4b188
amiiiiee <amiameen507@gmail.com>
2025-04-05 12:42:18 +0530

    edits

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -1,38 +1,32 @@
-import { lazy, Suspense, memo} from "react";
+import { lazy, Suspense, memo } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ScriptScenesList from "../components/ScriptScenesList";
-import ScriptWriting from "../pages/editor/ScriptWriting";
 import PrivateRoute from "../utils/UserProtectRouter";
-
-
-
-
 
 // Lazy-loaded components
 
 // const HomePage = lazy(() => import("../components/home/Home"));
-const ProjectPage = lazy(() => import("../components/project/Projects"))
+const ProjectPage = lazy(() => import("../components/project/Projects"));
 const SignupPage = lazy(() => import("../pages/auth/SignupPage"));
 const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
 const ProfileCreation = lazy(() => import("../pages/auth/ProfileCreation"));
-
-
+const ScriptScenesList = lazy(() => import("../components/ScriptScenesList"));
+const ScriptWriting = lazy(() => import("../pages/editor/ScriptWriting"));
 
 const AppRoutes = memo(() => {
   return (
     <Router>
-      <Suspense >
+      <Suspense>
         <Routes>
-          <Route path="/signup" element={<SignupPage/>} />
-          <Route path="/login" element={<LoginPage/>} />
-          <Route element={<PrivateRoute />}> 
-          <Route path="/create/profile" element={<ProfileCreation/>} />
-          {/* User Routes */}
-          {/* <Route path="/" element={<HomePage />} /> */}
-          <Route path="/" element={<ProjectPage />} />
-          <Route path="/scriptlists/:id" element={<ScriptScenesList />} />
-        <Route path="/scriptwriter" element={<ScriptWriting />} />
-        </Route>
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/create/profile" element={<ProfileCreation />} />
+            {/* User Routes */}
+            {/* <Route path="/" element={<HomePage />} /> */}
+            <Route path="/" element={<ProjectPage />} />
+            <Route path="/scriptlists/:id" element={<ScriptScenesList />} />
+            <Route path="/scriptwriter" element={<ScriptWriting />} />
+          </Route>
         </Routes>
       </Suspense>
     </Router>
