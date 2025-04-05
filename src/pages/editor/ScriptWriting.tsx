a32512a39a10bd31579eb1fce7a6b11c8979c942
amiiiiee <amiameen507@gmail.com>
2025-04-05 12:58:52 +0530

    edits

--- src/pages/editor/ScriptWriting.tsx
+++ src/pages/editor/ScriptWriting.tsx
@@ -21,20 +21,12 @@ const ScriptWriting:React.FC = () => {
           {/* Scene Action Button */}
 
           <div className="flex flex-col w-1/3 2xl:w-1/3 space-y-4 ">
-            <button className={`border border-black border-opacity-30 rounded-full px-4 py-2 ${
-            selectedButton === "Description" 
-              ? "bg-pink-100 text-black font-semibold" 
-              : "bg-blue-200 text-black font-medium"
-          }`}>Action</button>
+
             <p  className="text-center text-opacity-70 text-black">Press Keyboard Shift+Tab</p>
           </div>
 {/* Add Character Button */}
           <div className="flex flex-col w-1/3 2xl:w-1/3  space-y-4 ">
-            <button className={`border border-black border-opacity-30 rounded-full px-4 py-2 ${
-            selectedButton === "Characters" 
-              ? "bg-pink-100 text-black font-semibold" 
-              : "bg-blue-200 text-black font-medium"
-          }`}>Character</button>
+
             <p className="text-center text-opacity-70 text-black">Press Keyboard Tab</p>
           </div>
 
